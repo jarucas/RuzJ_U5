23badbb61e041aeaa1d8e7e61471acee486aa34b
Unknown <jarucas92@gmail.com>
2018-03-24 17:28:17 +0100

    All java converted to kotlin

---
+++ app/src/main/java/com/example/audiolibros/AdaptadorLibrosFiltro.kt
@@ -0,0 +1,72 @@
+package com.example.audiolibros
+
+import android.content.Context
+import java.util.*
+
+class AdaptadorLibrosFiltro(contexto: Context,
+                            private val vectorSinFiltro: MutableList<Libro>// Vector con todos los libros
+) : AdaptadorLibros(contexto, vectorSinFiltro) {
+    private var indiceFiltro: MutableList<Int>? = null // Índice en vectorSinFiltro de
+    // Cada elemento de listaLibros
+    private var busqueda = ""         // Búsqueda sobre autor o título
+    private var genero = ""           // Género seleccionado
+    private var novedad = false      // Si queremos ver solo novedades
+    private var leido = false        // Si queremos ver solo leidos
+
+    init {
+        recalculaFiltro()
+    }
+
+    fun setBusqueda(busqueda: String) {
+        this.busqueda = busqueda.toLowerCase()
+        recalculaFiltro()
+    }
+
+    fun setGenero(genero: String) {
+        this.genero = genero
+        recalculaFiltro()
+    }
+
+    fun setNovedad(novedad: Boolean) {
+        this.novedad = novedad
+        recalculaFiltro()
+    }
+
+    fun setLeido(leido: Boolean) {
+        this.leido = leido
+        recalculaFiltro()
+    }
+
+    fun recalculaFiltro() {
+        listaLibros = Vector()
+        indiceFiltro = Vector()
+        for (i in vectorSinFiltro.indices) {
+            val libro = vectorSinFiltro[i]
+            if ((libro.titulo.toLowerCase().contains(busqueda) || libro.autor.toLowerCase().contains(busqueda))
+                    && libro.genero.startsWith(genero)
+                    && (!novedad || novedad && libro.novedad)
+                    && (!leido || leido && libro.leido)) {
+                (listaLibros as Vector<Libro>).add(libro)
+                indiceFiltro!!.add(i)
+            }
+        }
+    }
+
+    fun getItem(posicion: Int): Libro {
+        return vectorSinFiltro[indiceFiltro!![posicion]]
+    }
+
+    override fun getItemId(posicion: Int): Long {
+        return indiceFiltro!![posicion].toLong()
+    }
+
+    fun borrar(posicion: Int) {
+        vectorSinFiltro.removeAt(getItemId(posicion).toInt())
+        recalculaFiltro()
+    }
+
+    fun insertar(libro: Libro) {
+        vectorSinFiltro.add(libro)
+        recalculaFiltro()
+    }
+}
---
+++ app/src/main/java/com/example/audiolibros/Aplicacion.kt
@@ -0,0 +1,41 @@
+package com.example.audiolibros
+
+import android.app.Application
+import android.graphics.Bitmap
+import android.util.LruCache
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.Volley
+
+class Aplicacion : Application() {
+
+    var listaLibros: List<Libro>? = null
+        private set
+    var adaptador: AdaptadorLibrosFiltro? = null
+        private set
+
+    var colaPeticiones: RequestQueue? = null
+        private set
+    var lectorImagenes: ImageLoader? = null
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        listaLibros = ejemploLibros()
+        adaptador = AdaptadorLibrosFiltro(this, listaLibros as MutableList<Libro>)
+        colaPeticiones = Volley.newRequestQueue(this)
+        lectorImagenes = ImageLoader(colaPeticiones,
+                object : ImageLoader.ImageCache {
+                    private val cache = LruCache<String, Bitmap>(10)
+
+                    override fun putBitmap(url: String, bitmap: Bitmap) {
+                        cache.put(url, bitmap)
+                    }
+
+                    override fun getBitmap(url: String): Bitmap {
+                        return cache.get(url)
+                    }
+                })
+    }
+
+}
